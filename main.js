b83f2485c80a3535c20a6229f8cd4922f4f19787
PatrickHickey <codewithpat@gmail.com>
2019-04-11 23:01:31 -0700

    Added nextSlide in mainJS

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ const prev = document.querySelector('#prev');
 const auto = false;
 const intervalTime = 5000;
 let slideInterval;
+
+const nextSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
